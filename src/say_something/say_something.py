41dede51756d9b07a39c085c849da430436d3a65
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-08-04 14:29:53 +0000

    onru

--- src/say_something/say_something.py
+++ src/say_something/say_something.py
@@ -10,14 +10,16 @@ def say(text = None, language = "en", no_cache = False, reset = False):
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-t", "--text", type=str, help="Text")
+    parser.add_argument("-t", "--text", type=str, nargs="+", help="Text")
     parser.add_argument('-l', '--language', type=str, help='Language')
     parser.add_argument('-nc', '--nocache', action="store_true", help='No cache')
     parser.add_argument('-r', '--reset', action="store_true", help='Reset (removing the caches)')
     args = parser.parse_args()
 
     if not args.text is None:
-        text = args.text
+        text = ""
+        for t in args.text:
+            text += f"{t} "
     if not args.language is None:
         language = args.language
     if args.nocache:
@@ -29,8 +31,6 @@ def say(text = None, language = "en", no_cache = False, reset = False):
     this_dir, this_filename = os.path.split(__file__)
     the_dir = os.path.join(this_dir, "cache")
     
-    
-
     if reset:
         for root, dirs, files in os.walk(the_dir):
             for file in files:
@@ -39,7 +39,8 @@ def say(text = None, language = "en", no_cache = False, reset = False):
         if args.text is None and args.reset:
             exit()
 
-    files = os.path.join(the_dir, f"{text[:10]}.mp3")
+    file_name = str(text[:20]).replace(" ", "_")
+    files = os.path.join(the_dir, f"{file_name}.mp3")
                 
     if not os.path.exists(files) or no_cache:
         gTTS(text, lang = language).save(files)
